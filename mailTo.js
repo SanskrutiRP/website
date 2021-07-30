eea53629d7e105b9446620ed0345a6d8fabd1510
Sanskruti <sanskrutipunde501@gmail.com>
2021-07-30 19:30:03 +0530

    Updated website

--- mailTo.js
+++ mailTo.js
@@ -1,7 +1,7 @@
 const open1 = document.getElementById("sendmail");
 open1.addEventListener('click', ()=> {
-    var email = document.getElementById('email');
-    var subject = document.getElementById('sub');
-    var emailBody = "Hi I am "+document.getElementById('name')+document.getElementById('msg');
+    var email = document.getElementById('email').value;
+    var subject = document.getElementById('sub').value;
+    var emailBody = "Hi I am "+document.getElementById('name').value+"<br>"+document.getElementById('msg').value;
     document.location = "mailto:"+email+"?subject="+subject+"&body="+emailBody;
 });
